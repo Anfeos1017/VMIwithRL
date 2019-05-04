11b2d4e16060f80c8c71832672b31829679c31d5
lhincapie0 <38962504+lhincapie0@users.noreply.github.com>
2019-05-04 17:26:38 -0500

    Clone method created.

--- ImplementacionQLearning/QLearning/src/model/QLearner.java
+++ ImplementacionQLearning/QLearning/src/model/QLearner.java
@@ -1,22 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
 
+import javafx.scene.shape.Path;
 import test.Laberinto;
 
-public class QLearner {
+public class QLearner  {
 
 	public LearningModel model;
 	private double[][] Qtable;
 	private double discountFactor;
 	private HashMap<String, Integer> stateMapping;
 	private HashMap<String, Integer> actionMapping;
-
+	
+	 
+	private ArrayList<LearningModel> models;
+	
 	public QLearner(LearningModel m, double discountFactor) {
 		this.model = m;
 		this.Qtable = new double[m.states.size()][m.actions.size()];
+		models = new ArrayList<LearningModel>();
 		this.discountFactor = discountFactor;
 
 		stateMapping = new HashMap<>();
@@ -62,6 +70,8 @@ public class QLearner {
 	}
 
 	private void printOptimaPolicy() {
+		
+		System.out.println("OPTIMAL POLICY");
 		boolean stoppedRun = false;
 		String currentState = this.model.initialState;
 		while (!stoppedRun) {
@@ -87,12 +97,58 @@ public class QLearner {
 		}
 	}
 
+	private void searchOptimalSolution()
+	{
+		
+		double max = Double.NEGATIVE_INFINITY;
+		int pos = 0;
+		String lastState ="";
+		System.out.println("OPTIMAL SOLUTION");
+		
+		boolean fin= false;
+			for(int i = 0;i<models.size() && !fin;i++)
+			{
+			if(models.get(i).lastState.equals("9,9"))
+			{
+				fin = true;
+			}
+			}
+			if(fin)
+			{
+				System.out.println("entro");
+
+					for(int i = 0;i<models.size();i++)
+					{
+					if(models.get(i).totalReward>max && models.get(i).lastState.equals("9,9"))
+					{
+						max = models.get(i).totalReward;
+						pos = i;
+						lastState = models.get(i).lastState;
+
+					}
+					else {
+							models.remove(i);
+							i--;
+					}
+				}
+					System.out.println("MAX VALUE = " +max +" Index: "+ pos + " LAST STATE"+ lastState);
+					//for(int i = 0; i<models.get(pos).path.size();i++)
+					//{
+						//System.out.println(models.get(pos).path.get(i));
+					//}
+		}
+			
+
+	}
+	
 	public void fit(int runs) {
 		Random rand = new Random();
 		for (int i = 0; i < runs; i++) {
+			model.path.clear();
+			double total = 0;
+			model.totalReward = 0;
 			System.out.println("Run # " + i);
 			String currentState = model.initialState;
-
 			boolean stoppedRun = false;
 
 			while (!stoppedRun) {
@@ -103,7 +159,9 @@ public class QLearner {
 					// Tomar la accion mas favorable (Explotation)
 
 					String nextAction = pickOptimalAction(this.model.transitionFunction.get(currentState));
+
 					if (nextAction != null) {
+						
 						double reward = this.model.getReward(currentState, nextAction);
 						String nextState = this.model.transitionFunction.get(currentState).get(nextAction);
 
@@ -120,7 +178,10 @@ public class QLearner {
 								.get(nextAction)];
 						this.Qtable[this.stateMapping.get(currentState)][this.actionMapping
 								.get(nextAction)] += this.discountFactor * (reward + max - qaux);
+						total +=discountFactor * (reward + max - qaux);
+
 						currentState = this.model.transitionFunction.get(currentState).get(nextAction);
+						model.path.add(currentState);
 						this.model.notifyCurrentState(currentState);
 						if (this.model.finalStates.contains(currentState)) {
 							stoppedRun = true;
@@ -150,10 +211,18 @@ public class QLearner {
 								.get(nextAction)];
 						this.Qtable[this.stateMapping.get(currentState)][this.actionMapping
 								.get(nextAction)] += this.discountFactor * (reward + max - qaux);
-						currentState = this.model.transitionFunction.get(currentState).get(nextAction);
+					 currentState = this.model.transitionFunction.get(currentState).get(nextAction);
+						model.path.add(currentState);
+
 						this.model.notifyCurrentState(currentState);
+						total +=discountFactor * (reward + max - qaux);
+
 						if (this.model.finalStates.contains(currentState)) {
 							stoppedRun = true;
+							System.out.println(currentState);
+							model.lastState = currentState;
+
+
 						}
 					} else {
 						stoppedRun = true;
@@ -161,9 +230,51 @@ public class QLearner {
 
 				}
 			}
+			System.out.println("TOTAL" +total);
+
+			model.totalReward = total;
+			saveModel( );
+			//printQTable();
 
 		}
-this.printOptimaPolicy();
+
+		
+		//*** LO BLOQUIE PARA MIRARLO SIN QUE ENTRE EN EL BUCLE
+		//this.printOptimaPolicy();
+		searchOptimalSolution();
+		((Laberinto)this.model).imprimirLaberinto();
+
 	}
+	
+	
+	public void printQTable() {
+		for (int k = 0; k < Qtable.length; k++) {
+			for (int l = 0; l < Qtable[0].length; l++) {
+				System.out.print(Qtable[k][l] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void printModels()
+	{
+		for(int i = 0; i<models.size();i++)
+		{
+			
+				System.out.println(models.get(i).totalReward);
+				System.out.println(models.get(i).lastState);
+
+			
+
+		}
+	}
+	
+	public void saveModel()
+	{
+		models.add((LearningModel) model.clone());
+	}
+	
+	
+	
 
 }
