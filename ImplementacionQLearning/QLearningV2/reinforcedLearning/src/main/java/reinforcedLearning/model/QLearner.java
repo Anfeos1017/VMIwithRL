66388d7d7abf87b4ab13660b3eb604f5c33fdd86
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2019-05-06 23:53:24 -0500

    Added policy test method

--- ImplementacionQLearning/QLearningV2/reinforcedLearning/src/main/java/reinforcedLearning/model/QLearner.java
+++ ImplementacionQLearning/QLearningV2/reinforcedLearning/src/main/java/reinforcedLearning/model/QLearner.java
@@ -1,5 +1,6 @@
 package reinforcedLearning.model;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import java.util.Random;
@@ -56,6 +57,52 @@ public class QLearner<M extends AbstractModel<S, A>, S extends State, A extends 
 			System.out.println(toPrint);
 		}
 	}
+	
+	public double runPolicyTest() throws Exception {
+		M model = (M) this.model.getInitialSystem();
+		double cumReward = 0;
+		boolean stoppedRun = false;
+		while(!stoppedRun) {
+
+			A nextAction = this.pickOptimalAction(model.currentState, model);
+
+			if (nextAction != null) {
+				double reward = model.getReward(model.currentState, nextAction);
+				cumReward += reward;
+				S nextState = model.getNextState(model.currentState, nextAction);
+				//System.out.println(model.currentState.name+" + "+nextAction.name+" => "+nextState.name +" ; "+ cumReward+" ; "+this.Qtable[this.stateMapping.get(model.currentState.name)][this.actionMapping.get(nextAction.name)]);
+				double max = Double.NEGATIVE_INFINITY;
+
+				for (A action : model.allowedActions.get(nextState.name)) {
+					if (this.Qtable[this.stateMapping.get(nextState.name)][this.actionMapping
+							.get(action.name)] > max) {
+						max = this.Qtable[this.stateMapping.get(nextState.name)][this.actionMapping
+								.get(action.name)];
+
+					}
+				}
+
+				double qaux = this.Qtable[this.stateMapping.get(model.currentState.name)][this.actionMapping
+						.get(nextAction.name)];
+				this.Qtable[this.stateMapping.get(model.currentState.name)][this.actionMapping
+						.get(nextAction.name)] += this.learningRate * (reward + (this.discountFactor*(max - qaux)));
+
+				model.setAsCurrentState(model.takeAction(model.currentState, nextAction));
+				if (model.finalStates.containsKey(model.currentState.name)) {
+					stoppedRun = true;
+					// System.out.println("Reached final State on Step: "+step);
+				}
+			} else {
+				stoppedRun = true;
+				// System.out.println("Reached dead State on Step: "+step);
+			}
+
+		
+		}
+		return cumReward;
+	}
+	
+	
 
 	public void fitModel(int runs) throws Exception {
 		Random rand = new Random();
@@ -68,7 +115,7 @@ public class QLearner<M extends AbstractModel<S, A>, S extends State, A extends 
 			int explotation=0;
 			while (!stoppedRun) {
 				// System.out.println(model.currentState.name);
-				if (rand.nextDouble() <= (i / (double) runs)) {
+				if (rand.nextDouble() <= ((i+1) / ((double) runs))) {
 					// EXPLOTATION
 					totalStep++;
 					explotation++;
@@ -149,7 +196,7 @@ public class QLearner<M extends AbstractModel<S, A>, S extends State, A extends 
 			// "+this.Qtable[this.Qtable.length-1][this.Qtable[0].length-1]);
 			//System.out.println((i)+" : "+ "Explotation:"+(explotation/(double)totalStep));
 		}
-		this.printQTable();
+		//this.printQTable();
 	}
 
 }
